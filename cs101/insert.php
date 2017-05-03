2f8658973d45f4a39fc0f02d58e7811682555d7d
preetha2711 <datta.preetha@gmail.com>
2017-05-04 01:10:06 +0530

    changing php file for more attributes

--- cs101/insert.php
+++ cs101/insert.php
@@ -11,8 +11,12 @@ $link = mysqli_connect("localhost", "root", "", "vistaar");
 $name = mysqli_real_escape_string($link, $_REQUEST['name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $date = mysqli_real_escape_string($link, $_REQUEST['date']);
+$time_in = mysqli_real_escape_string($link, $_REQUEST['time_in']);
+$time_out = mysqli_real_escape_string($link, $_REQUEST['time_out']);
+$instrument = mysqli_real_escape_string($link, $_REQUEST['instrument']);
 
-$sql = "INSERT INTO appointments (name,email, date) VALUES ('$name', '$email', '$date')";
+
+$sql = "INSERT INTO appointments (name,email, date, time_in, time_out, instrument) VALUES ('$name', '$email', '$date', '$time_in', '$time_out', 'instrument')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
