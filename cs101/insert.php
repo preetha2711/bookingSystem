5492d4cd75fa55f6732bd76f3619e69e41d84450
preetha2711 <datta.preetha@gmail.com>
2017-05-06 17:20:51 +0530

    added time out calculation to insert.php

--- cs101/insert.php
+++ cs101/insert.php
@@ -7,16 +7,18 @@ $link = mysqli_connect("localhost", "root", "", "vistaar");
  if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+//Calculaitng the time out for the music room - only one hour slots allowed
+$time_out = $_POST['time_in']+1;
+
  // MySQL INJECTION
 $name = mysqli_real_escape_string($link, $_REQUEST['name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $date = mysqli_real_escape_string($link, $_REQUEST['date']);
 $time_in = mysqli_real_escape_string($link, $_REQUEST['time_in']);
-$time_out = mysqli_real_escape_string($link, $_REQUEST['time_out']);
 $instrument = mysqli_real_escape_string($link, $_REQUEST['instrument']);
 
 
-$sql = "INSERT INTO appointments (name,email, date, time_in, time_out, instrument) VALUES ('$name', '$email', '$date', '$time_in', '$time_out', 'instrument')";
+$sql = "INSERT INTO appointments (name,email, date, time_in, time_out, instrument) VALUES ('$name', '$email', '$date', '$time_in', 'time_out', instrument')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
